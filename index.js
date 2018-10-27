4aab0894360ae1ae125e5cba24fdb4a08259ecc3
VITOR SOARES SILVA <vitor.soares@portoseguro.com.br>
2018-10-27 19:47:21 -0300

    adding chapters mapping

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var request = require('request'),
     fs = require('fs'),
     config = require('./config.json');
 
-function downloadIndexUrl(indexUrl, fileName) {
+function downloadIndexUrl(indexUrl, metadataFile, fileName) {
     var spawn = require('child_process').spawn,
-        ffmpeg = spawn(config.ffmpegPath, ['-i', indexUrl, '-c', 'copy', fileName + '.mp4']);
+        ffmpeg = spawn(config.ffmpegPath, ['-i', indexUrl, '-i', metadataFile, '-map_chapters', '1', '-c', 'copy', fileName + '.mp4']);
     ffmpeg.stdout.on('data', function (data) {
         console.log('stdout: ' + data.toString());
     });
@@ -17,6 +17,7 @@ function downloadIndexUrl(indexUrl, fileName) {
 
     ffmpeg.on('exit', function (code) {
         console.log('child process exited with code ' + code.toString());
+        fs.unlinkSync(metadataFile);
     });
 }
 
@@ -53,7 +54,10 @@ request.get(args[0], {}, function (err, response) {
                         i,
                         resolution,
                         curResolution,
-                        indexUrl;
+                        indexUrl,
+                        metadataFile = vilosConfig.metadata.id + '.metadata',
+                        metadataContent = [],
+                        adChapterOffset = parseInt(config.adChapterOffset || "20000");
                     for (i = 1; i < lines.length; i += 2) {
                         if (lines[i] && lines[i].trim()) {
                             resolution = parseInt(/(RESOLUTION=)([0-9]+)/g.exec(lines[i])[2]);
@@ -63,7 +67,31 @@ request.get(args[0], {}, function (err, response) {
                             }
                         }
                     }
-                    downloadIndexUrl(indexUrl, fileName);
+                    metadataContent.push(";FFMETADATA1\ntitle=");
+                    metadataContent.push(vilosConfig.metadata.title);
+                    metadataContent.push("\n")
+                    for (i = 0; i < vilosConfig.ad_breaks.length; i++) {
+                        var ad_break = parseInt(vilosConfig.ad_breaks[i].offset) - adChapterOffset,
+                            next_ad_break = vilosConfig.ad_breaks[i + 1];
+                        if (ad_break < 0) {
+                            ad_break = 0;
+                        }
+                        if (next_ad_break) {
+                            next_ad_break = parseInt(next_ad_break.offset) - (adChapterOffset + 1);
+                        } else {
+                            next_ad_break = parseInt(vilosConfig.metadata.duration);
+                        }
+
+                        metadataContent.push("[CHAPTER]\nTIMEBASE=1/1000\nSTART=");
+                        metadataContent.push(ad_break);
+                        metadataContent.push("\nEND=");
+                        metadataContent.push(next_ad_break);
+                        metadataContent.push("\n");
+                    }
+                    metadataContent.push("[STREAM]\ntitle=");
+                    metadataContent.push(vilosConfig.metadata.title);
+                    fs.writeFileSync(metadataFile, metadataContent.join(''));
+                    downloadIndexUrl(indexUrl, metadataFile, fileName);
                 }
             });
         }
